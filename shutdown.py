4e91d3ed5c95b7c8013185ce1c1e8b48e3336720
alcsmadia <nintendwu@gmail.com>
2018-08-25 15:48:10 +0900

    仕組み変えた

--- shutdown.py
+++ shutdown.py
@@ -3,22 +3,19 @@ import sys
 import time
 
 dir = os.path.dirname(os.path.abspath(__name__)) # スクリプトのあるディレクトリの絶対パスを取得
-exist = 0
+exist = 1
 
-# ------ ダウンロード中のファイルを探す -------
-for file in os.listdir(dir):
-    print(file)
-    base, ext = os.path.splitext(file)
-    if ext == '.crdownload':
-        exist = 1
-        print("File is founded")
-        filename = base + ext
-
-# ------ ファイルがなくなるまでループ ---------
-while exist == 1:
+# ------ ダウンロード中のファイルがなくなるまでループ ---------
+while exist >= 1:
+    exist = 0
+    for file in os.listdir(dir):
+        print(file)
+        base, ext = os.path.splitext(file)
+        if ext == '.crdownload':
+            exist += 1
+            print("File is founded")
+            filename = base + ext
     print("Waiting...")
     time.sleep(2)
-    if not os.path.exists(dir + "\\" + filename):
-        exist = 0
 
 os.system('shutdown -s -f')
