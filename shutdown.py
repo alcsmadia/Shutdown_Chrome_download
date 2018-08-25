e7b825ea820330cbc67372d86a49afbf7471ee4d
alcsmadia <nintendwu@gmail.com>
2018-08-25 15:37:11 +0900

    動かなかったので1ファイルしかできないようにした

--- shutdown.py
+++ shutdown.py
@@ -5,23 +5,20 @@ import time
 dir = os.path.dirname(os.path.abspath(__name__)) # スクリプトのあるディレクトリの絶対パスを取得
 exist = 0
 
-while exist == 0: # n個ファイルダウンロードしてるときn回ループ
-    # ------ ダウンロード中のファイルを探す -------
-    for file in os.listdir(dir):
-        base, ext = os.path.splitext(file)
-        if ext == '.crdownload':
-            exist = 1
-            print("File is founded")
-            filename = base + ext
-    if exist == 0:
-        print("Download file is not founded")
-        time.sleep(20)
-        os.system('shutdown -s -f')
-        sys.exit() # 終了
+# ------ ダウンロード中のファイルを探す -------
+for file in os.listdir(dir):
+    print(file)
+    base, ext = os.path.splitext(file)
+    if ext == '.crdownload':
+        exist = 1
+        print("File is founded")
+        filename = base + ext
 
-    # ------ ファイルがなくなるまでループ ---------
-    while exist == 1:
-        print("Waiting...")
-        time.sleep(30)
-        if not os.path.exists(dir + "\\" + filename):
-            exist = 0
+# ------ ファイルがなくなるまでループ ---------
+while exist == 1:
+    print("Waiting...")
+    time.sleep(2)
+    if not os.path.exists(dir + "\\" + filename):
+        exist = 0
+
+os.system('shutdown -s -f')
