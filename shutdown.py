16e726318789a02987cbe6a01206f76813485b55
alcsmadia <nintendwu@gmail.com>
2018-08-25 17:10:22 +0900

    うごかなかった

--- shutdown.py
+++ shutdown.py
@@ -3,18 +3,19 @@ import sys
 import time
 
 dir = os.path.dirname(os.path.abspath(__name__)) # スクリプトのあるディレクトリの絶対パスを取得
-exist = 1
 
 # ------ ダウンロード中のファイルがなくなるまでループ ---------
-while exist >= 1:
-    exist = 0
+while True:
+    time.sleep(30)
+    n = 0
     for file in os.listdir(dir):
         base, ext = os.path.splitext(file)
         if ext == '.crdownload':
-            exist += 1
+            n += 1
             print("File is founded")
             filename = base + ext
     print("Waiting...")
-    time.sleep(30)
+    if n == 0:
+        break
 
 os.system('shutdown -s -f')
